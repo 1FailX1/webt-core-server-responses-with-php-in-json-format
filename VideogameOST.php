b8cafb8ffb3152cfd5a6df7a05c6133ccbaf33f5
1FailX1 <1felixpayer1@gmail.com>
2023-10-18 10:30:56 +0200

    DocBlock added VideogameOST.php
    Added DocBlock

--- VideogameOST.php
+++ VideogameOST.php
@@ -10,6 +10,14 @@ class VideogameOST
     public int $releaseYear;
     public $trackList;
 
+    /**
+     * @param int $ID ID of OST
+     * @param string $name name of OST
+     * @param string $videoGameName name of OST
+     * @param int $releaseYear year of the game's release
+     * @param $trackList array containing all of the OST's tracks
+     */
+
     function __construct(int $ID, string $name, string $videoGameName, int $releaseYear, $trackList)
     {
         $this->ID = $ID;
