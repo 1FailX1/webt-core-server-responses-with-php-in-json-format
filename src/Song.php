c72167782b3de1943bb9f52baa6731f3e3ca94e7
JakobDrescher <jakobdrescher6@gmail.com>
2023-10-18 10:27:21 +0200

    created the class Song

---
+++ src/Song.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+class Song
+{
+    private int $id;
+    private string $name;
+    private string $artist;
+    private int $trackNumber;
+    private int $duration;
+
+    /**
+     * @param int $id a unique ID
+     * @param string $name the song title
+     * @param string $artist the name of the artist
+     * @param int $trackNumber the track number
+     * @param int $duration the duration of the song in seconds
+     */
+    public function __construct(int $id, string $name, string $artist, int $trackNumber, int $duration)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->artist = $artist;
+        $this->trackNumber = $trackNumber;
+        $this->duration = $duration;
+    }
+}
