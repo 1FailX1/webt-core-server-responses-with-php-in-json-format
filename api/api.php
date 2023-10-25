bab2fcef6b81351d8be9fbd23dda5e0e13f035df
Hunor Zakarias <0158@htl.rennweg.at>
2023-10-25 09:12:23 +0200

    added header to api.php reformated index.php

--- api/api.php
+++ api/api.php
@@ -5,5 +5,5 @@ require "../src/OSTSeeder.php";
 
 $id = $_GET["ostID"];
 
-
+header('Content-Type: application/json');
 echo json_encode(OSTSeeder::getOSTs()[$id]);
--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 </head>
 
 <body>
-    <form method="get" action="/api/api.php">
+    <form method="get" action="./api/api.php">
         <select id="ostSelection" name="ostID">
             <option value=1>Doom</option>
             <option value=2>Crypt of the Necrodancer</option>
             <option value=3>UNDERTALE</option>
         </select>
 
-        <input type="submit" value="submit oida">
+        <input type="submit" value="submited">
     </form>
 </body>
 
